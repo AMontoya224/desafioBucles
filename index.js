92f67faa3fb22431983857594219b5a39249d843
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-01-07 19:34:43 -0500

    Prueba de bucles

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-console.log("Imprimir impares 1-20");
+console.log("1. Imprimir impares 1-20");
 for (let i=1; i<=20; i++){
     if (i % 2 !== 0){
         console.log(i);
     }
 }
 
-console.log("Disminuir múltiplos de 3");
+console.log("2. Disminuir múltiplos de 3");
 for (let i=100; i>=0; i--){
     if (i % 3 === 0){
         console.log(i);
     }
 }
 
-console.log("Imprime la secuencia");
+console.log("3. Imprime la secuencia");
 let i = 4;
 while(i>-4) {
     console.log(i);
     i = i-1.5;
 }
 
-console.log("Sigma");
+console.log("4. Sigma");
 let sum = null;
 for (let i=1; i<=100; i++){
     sum = sum + i;
 }
 console.log(sum);
 
-console.log("Factorial");
+console.log("5. Factorial");
 let product = 1;
 for (let i=1; i<=12; i++){
     product = product * i;
